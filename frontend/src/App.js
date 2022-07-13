2499583a1ea72406dbd71d96dd79f7ee65320829
Naresh <wdev6666@gmail.com>
2022-07-13 08:24:55 +0530

    Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Profile from "./pages/profile/Profile";
-
 import {
   BrowserRouter as Router,
   Routes,
